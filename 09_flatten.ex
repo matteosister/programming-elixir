5b0239278470b0a5435f265536c1052761e099c0
Matteo <matteog@gmail.com>
2015-04-22 00:33:29 +0200

    flatten

---
+++ 09_flatten.ex
@@ -0,0 +1,8 @@
+defmodule MyList do
+  def flatten(list), do: _flatten(list, [])
+  defp _flatten([head | tail], acc) when is_list(head), do: _flatten(head, acc) ++ _flatten(tail, [])
+  defp _flatten([head | tail], acc) do
+    _flatten(tail, acc ++ [head])
+  end
+  defp _flatten([], acc), do: acc
+end
